08a172d8933891f727e6f9ce126d8b3dd4a816a3
mjordan <michael@glowingpixel.com>
2020-06-20 00:12:00 -0700

    Added column hiding (in progress)

--- ui/view_alldailies.py
+++ ui/view_alldailies.py
@@ -9,8 +9,15 @@ class AllDailiesView(QtWidgets.QWidget):
 		self.setupWidgets()
 	
 	def setupWidgets(self):
+		self.lay_top = QtWidgets.QHBoxLayout()
+		self.lay_main.addLayout(self.lay_top)
 		self.lbl_summary = QtWidgets.QLabel("No show selected")
-		self.lay_main.addWidget(self.lbl_summary)
+		self.lbl_summary.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed))
+		self.lay_top.addWidget(self.lbl_summary)
+		self.btn_choosecolumns = QtWidgets.QPushButton("Choose Columns")
+		self.btn_choosecolumns.clicked.connect(self.chooseColumns)
+		self.lay_top.addWidget(self.btn_choosecolumns)
+		
 		self.tree_alldailies = QtWidgets.QTreeView()
 		self.lay_main.addWidget(self.tree_alldailies)
 	
@@ -35,3 +42,43 @@ class AllDailiesView(QtWidgets.QWidget):
 			item_root.appendRow([QtGui.QStandardItem(x) for x in row])
 		
 		self.tree_alldailies.setModel(self.model_dailies)
+		self.lbl_summary.setText(f"Showing all {self.model_dailies.rowCount():,} shots")
+	
+	def chooseColumns(self):
+		self.wnd_choosecolumns = ColumnChooser(self, [(not self.tree_alldailies.isColumnHidden(x), self.columns[x]) for x in range(len(self.columns))])
+		if self.wnd_choosecolumns.exec_():
+			self.setVisibleColumns([x.row() for x in self.wnd_choosecolumns.list_columns.selectedIndexes()])
+	
+	def setVisibleColumns(self, columns):
+		for col in range(len(self.columns)):
+			self.tree_alldailies.setColumnHidden(col, col not in columns)
+
+class ColumnChooser(QtWidgets.QDialog):
+
+	def __init__(self, parent, columns):
+		super().__init__(parent)
+		self.setModal(True)
+		self.visibleColumns = []
+
+		self.lay_main = QtWidgets.QVBoxLayout()
+		self.setLayout(self.lay_main)
+
+		self.list_columns = QtWidgets.QListWidget()
+		self.list_columns.setSelectionMode(QtWidgets.QAbstractItemView.SelectionMode.MultiSelection)
+		{self.list_columns.addItem(x[1]) for x in columns}
+		{self.list_columns.item(x).setSelected(columns[x][0]) for x in range(len(columns))}
+		self.lay_main.addWidget(self.list_columns)
+
+		self.box_save = QtWidgets.QDialogButtonBox()
+		self.btn_ok = QtWidgets.QPushButton("Apply")
+		self.btn_ok.setDefault(True)
+		self.btn_ok.clicked.connect(self.accept)
+		self.btn_cancel = QtWidgets.QPushButton("Cancel")
+		self.btn_cancel.clicked.connect(self.reject)
+		self.box_save.addButton(self.btn_ok, QtWidgets.QDialogButtonBox.ButtonRole.ApplyRole)
+		self.box_save.addButton(self.btn_cancel, QtWidgets.QDialogButtonBox.ButtonRole.RejectRole)
+		self.lay_main.addWidget(self.box_save)
+
+	def returnSelectedColumns(self):
+#		self.visibleColumns = self.list_columns.selectedIndexes()
+		self.accept()
